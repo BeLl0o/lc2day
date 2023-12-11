f30857c8d8341f10d6f971e062274d5fac8d6d2f
BeLl0o <siyuma__@outlook.com>
2023-12-10 16:43:36 -0800

    learn java

---
+++ LC.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class LC{
+    //704.Binaray Search
+    public int search(int[] nums, int target){
+        
+        int l = 0, r = nums.length - 1, mid, res = -1;
+        while(l <= r){
+            mid = (l + r) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                l = mid + 1;
+            }else{
+                r = mid - 1;
+            }
+
+        }
+
+        return res;
+    }
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the target number");
+        int target = in.nextInt();
+        System.out.println("Enter the nums array");
+        in.nextLine();
+        String[] split = in.nextLine().split(" ");
+        in.close();
+        int[] nums = new int[split.length];
+        for(int i = 0; i < split.length; i++){
+            nums[i] = Integer.parseInt(split[i]);
+        }
+        LC test = new LC();
+        int res = test.search(nums, target);
+        System.out.println(res);
+        
+    }
+}
